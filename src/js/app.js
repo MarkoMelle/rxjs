2082f2b3ef9d2fdbb16b1a9ace0e85b4b2904a0c
Marko Melle <markmelle1@gmail.com>
2023-03-21 18:54:00 +0100

    app.js Messages.js main.yml changed

--- src/js/app.js
+++ src/js/app.js
@@ -1,27 +1,25 @@
-
 import { of, interval } from 'rxjs';
-import { catchError, pluck, mergeMap, take, map } from 'rxjs/operators';
+import {
+  catchError, pluck, mergeMap, take,
+} from 'rxjs/operators';
 import { ajax } from 'rxjs/ajax';
 import Messages from './Messages';
 
 document.addEventListener('DOMContentLoaded', () => {
   const messages = new Messages(document.querySelector('.app'));
-  const url = 'https://fake-message.onrender.com'
+  const url = 'https://fake-message.onrender.com';
   messages.render();
   const interval$ = interval(5000);
   interval$
     .pipe(
       take(5),
-      mergeMap(() => {
-        return ajax.getJSON(url + '/messages/unread')
-          .pipe(
-            catchError(err => {
-              console.log(err);
-              return of();
-            }),
-          )
-      }),
+      mergeMap(() => ajax.getJSON(`${url}/messages/unread`)
+        .pipe(
+          catchError((error) => {
+            console.error('Error occurred while fetching unread messages:', error);
+            return of(null);
+          }),
+        )),
       pluck('messages'),
-    ).subscribe(messages.add)
+    ).subscribe(messages.add);
 });
-
